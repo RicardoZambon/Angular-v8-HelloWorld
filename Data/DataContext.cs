24901e81dc5a0c905036e24d838a7dcd91131ff2
Ricardo Zambon <ricardo.zambon@hotmail.com>
2019-07-16 19:29:22 -0300

    Just adding DbContext and a simple model... Will retake this later.

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using AngularHelloWorld.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularHelloWorld.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,10 @@
+using AngularHelloWorld.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,9 +22,13 @@ namespace AngularDemoApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite("");
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-            // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
